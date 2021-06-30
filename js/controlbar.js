cc25bafd1e213b9b312b4d77d79b1abf45381a31
Jacob Borg <48691704+JacobBorgProgramming@users.noreply.github.com>
2021-06-29 21:54:20 -0700

    update

--- js/controlbar.js
+++ js/controlbar.js
@@ -1,105 +1,64 @@
-document.getElementById("item1").innerHTML =
-  localStorage["item1name"] || ""; // default text
+document.getElementById("item1name").value =
+  localStorage["item1name"] || ""; // 
 
-    document.getElementById("item1input").value =
-  localStorage["item1input"] || ""; 
+  document.getElementById("item1").innerHTML =
+  localStorage["item1"] || ""; // 
 
+  document.getElementById("item1input").value =
+  localStorage["item1input"] || ""; // 
 
-
-
-
-function inputitems(){
-    localStorage["item1input"] = document.getElementById("item1input").value; 
-    localStorage["item1name"] = document.getElementById("item1").innerHTML;
-
-}
-
-function saveitems(){
-    document.getElementById("item1input").value = localStorage["item1input"]; 
-    document.getElementById("item1").innerHTML = localStorage["item1name"];
-
-}
-
-
-
-
-
-function setitem1() {
-    var x1 = document.getElementById("item1input").value;
-
-    localStorage["item1input"] = document.getElementById("item1input").value; 
-  }
-
-  function setitem1name() {
-    var y1 = document.getElementById("item1name").value;
-    document.getElementById("item1").innerHTML = y1;
-
-    localStorage["item1name"] = document.getElementById("item1").innerHTML; // heading div
-    localStorage["item1name"] = document.getElementById("item1name").value;
-  }
-  function openitem1(){
-    var z1 = document.getElementById("item1input").value;
-    
-    
-    document.getElementById("appwindow").src = z1;
-    console.log(x1)
-    
-    
-}
-/*document.getElementById("item1").href = x;*/
-
-
-
-
-
-
+  document.getElementById("item1link").href =
+  localStorage["item1input"] || ""; // 
 
 
 setInterval(function() {
-  // fuction that is saving the innerHTML of the div
-  localStorage["item1name"] = document.getElementById("item1").innerHTML; // heading div
 
-  localStorage["item1name"] = document.getElementById("item1name").value; // heading div
-
-  localStorage["item1input"] = document.getElementById("item1input").value; 
-
-
+  localStorage["item1name"] = document.getElementById("item1name").value; // 
+  localStorage["item1"] = document.getElementById("item1").innerHTML; // 
+  localStorage["item1input"] = document.getElementById("item1input").value; // 
+  localStorage["item1input"] = document.getElementById("item1link").href; // 
+ 
 }, 1000);
 
 
+function save1(){
+  document.getElementById("item1").innerHTML = document.getElementById("item1name").value;
 
+}
 
-
-
-
-
-
-
-
+function openitem1(){
+  var z1 = document.getElementById("item1input").value;
+  
+  
+  document.getElementById("appwindow").src = z1;
+  console.log(x1)
+  
+  
+}
 
 
 
 function showsettings(){
-    document.getElementById("controlbarsettings").style.visibility = "visible";
+  document.getElementById("controlbarsettings").style.visibility = "visible";
 }
 function hidesettings(){
-    document.getElementById("controlbarsettings").style.visibility = "hidden";
+  document.getElementById("controlbarsettings").style.visibility = "hidden";
 }
 
 function settingsbuttonshow2(){
-    document.getElementById("settingsbutton2").style.visibility = "visible";
+  document.getElementById("settingsbutton2").style.visibility = "visible";
 }
 
 function settingsbuttonhide2(){
-    document.getElementById("settingsbutton2").style.visibility = "hidden";
-    
+  document.getElementById("settingsbutton2").style.visibility = "hidden";
+  
 }
 
 function settingsbuttonshow1(){
-    document.getElementById("settingsbutton1").style.visibility = "visible";
+  document.getElementById("settingsbutton1").style.visibility = "visible";
 }
 
 function settingsbuttonhide1(){
-    document.getElementById("settingsbutton1").style.visibility = "hidden";
+  document.getElementById("settingsbutton1").style.visibility = "hidden";
 }
 
