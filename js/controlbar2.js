7ebb9ea7dfc842830b6a29b452d7ff19b9a5842b
Jacob Borg <48691704+JacobBorgProgramming@users.noreply.github.com>
2021-06-29 20:20:25 -0700

    re construct

---
+++ js/controlbar2.js
@@ -0,0 +1,59 @@
+document.getElementById("item1name").value =
+  localStorage["item1name"] || ""; // 
+
+  document.getElementById("item1").innerHTML =
+  localStorage["item1"] || ""; // 
+
+  document.getElementById("item1input").innerHTML =
+  localStorage["item1input"] || ""; // 
+
+
+setInterval(function() {
+
+  localStorage["item1name"] = document.getElementById("item1name").value; // 
+  localStorage["item1"] = document.getElementById("item1").innerHTML; // 
+  localStorage["item1input"] = document.getElementById("item1input").value; // 
+ 
+}, 1000);
+
+
+function save1(){
+  document.getElementById("item1").innerHTML = document.getElementById("item1name").value;
+
+}
+
+function openitem1(){
+  var z1 = document.getElementById("item1input").value;
+  
+  
+  document.getElementById("appwindow").src = z1;
+  console.log(x1)
+  
+  
+}
+
+
+
+function showsettings(){
+  document.getElementById("controlbarsettings").style.visibility = "visible";
+}
+function hidesettings(){
+  document.getElementById("controlbarsettings").style.visibility = "hidden";
+}
+
+function settingsbuttonshow2(){
+  document.getElementById("settingsbutton2").style.visibility = "visible";
+}
+
+function settingsbuttonhide2(){
+  document.getElementById("settingsbutton2").style.visibility = "hidden";
+  
+}
+
+function settingsbuttonshow1(){
+  document.getElementById("settingsbutton1").style.visibility = "visible";
+}
+
+function settingsbuttonhide1(){
+  document.getElementById("settingsbutton1").style.visibility = "hidden";
+}
